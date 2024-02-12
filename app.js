6ba9ad5c789caa3e98cf6105b43fb4563d89a144
Legend <rattansingh1699@gmail.com>
2024-02-12 11:46:45 +0530

    socket io in progress

--- app.js
+++ app.js
@@ -1,66 +1,85 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-require('dotenv').config()
-const multer = require('multer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const multer = require("multer");
+const { Server } = require("socket.io");
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, getRndInteger(0, Number.MAX_SAFE_INTEGER) + '-' + file.originalname);
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      getRndInteger(0, Number.MAX_SAFE_INTEGER) + "-" + file.originalname
+    );
+  },
+});
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 // app.use(bodyParser.urlencoded()); //x-www-form-urlencoded
 
 app.use(bodyParser.json()); //application/json
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
 
 app.use((error, req, res, next) => {
-    // console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({ message: message, data: data });
-})
+  // console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
 
-mongoose.connect(process.env.DATABASE_URL)
-    .then(() => {
-        app.listen(8888);
-    }).catch((err) => {
-        console.log(err);
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    const server = app.listen(8888);
+    const io = new Server(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected " + socket.id);
     });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
